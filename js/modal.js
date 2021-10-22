dc114d768e5d806cb034fc7ab90debd4cf80a3e7
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-10-22 00:13:05 +0000

    ✅ Modal: complete

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modalElement = document.getElementById("model");
+let modalOverlay = document.getElementById("overlay");
+
+
+openModal.addEventListener("click", function() {
+    modalOverlay.style.display = "block";
+});
+
+
+
+
+closeModal.addEventListener("click", function() {
+    modalOverlay.style.display = "none";
+});
