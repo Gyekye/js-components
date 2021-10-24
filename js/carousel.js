2d6690753a8c3726f6c09a7c0ff92c8e387a9b58
Gyekye <mrgyekye740@gmail.com>
2021-10-24 22:33:01 +0000

    carousel complete

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,37 @@
-const getBodyTag = document.getElementsByTagName("Body");
-for(let i=0;i<getBodyTag.length;i++){
-    getBodyTag[i].textContent = "js working";
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('carousel-button-previous').addEventListener('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
