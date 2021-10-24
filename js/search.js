bbdda89f3c0ed5d419d56bb16288f1595707b2d5
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-10-24 14:20:06 +0000

    searchEngine Done

--- js/search.js
+++ js/search.js
@@ -1,7 +1,17 @@
-let username = document.getElementById("usernameInput");
+let username = document.getElementById("searchInput");
+let allNamesDomCollection = document.getElementsByClassName("name");
 
 
 username.addEventListener("keyup", function(e) {
-    let username = e.target.value.toLowerCase();
-    //console.log(username);
-})
+    let searchQuery = e.target.value.toLowerCase();
+    for (let i = 0; i < allNamesDomCollection.length; i++) {
+        const currentName = allNamesDomCollection[i].textContent.toLowerCase();
+
+
+        if (currentName.includes(searchQuery)) {
+            allNamesDomCollection[i].style.display = "block";
+        } else {
+            allNamesDomCollection[i].style.display = "none";
+        }
+    };
+});
